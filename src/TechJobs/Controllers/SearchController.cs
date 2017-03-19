7d0294383dfdc3b5bef78d057386fb6720a90a1f
Jill Shaddy-Gouvion <jshaddy75@gmail.com>
2017-03-19 12:36:47 -0500

    Complete TechConsole MVC Assignment

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,8 +13,25 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            if (searchType == "all")
+            {
+                jobs = JobData.FindByValue(searchTerm);
+            }
+            else
+             {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+             }
+  
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search Results";
+            return View("Index");
+        }
 
     }
+
+
 }
